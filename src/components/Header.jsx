8a744a9281652e493bd450b74e8fdc4c18c65d82
damnfri <subtobug@gmail.com>
2021-02-19 00:42:18 +0200

    new files

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return(
+        <header className ="header">
+        <img src = "https://cdn.mos.cms.futurecdn.net/BVb3Wzn9orDR8mwVnhrSyd-970-80.jpg.webp"></img>
+        </header>
+    )
+}
+export default Header;
+  
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+
+
+const Profile = () => {
+    return(
+      <div className = "content">
+      <img src ="https://miro.medium.com/max/700/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" width ="100%" height ="80px"></img>
+      
+      <div>
+      ava+ description
+      </div>
+      <div>
+      My Post
+      <div>
+        new post
+      </div>
+      <div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>
+      </div>
+      </div>
+    )
+}
+export default Profile;
